d1d27dd7922975b4445963fe39a1d7c2874474a6
Franco Cruces <francocrucesayala@gmail.com>
2017-10-28 18:34:01 -0300

    Request accepting constraints implemented #3 and some refactoring.
    Requests accept tag constraints. Model related files refactored into "model" package.

--- model/Menu.py
+++ model/Menu.py
@@ -1,8 +1,12 @@
+import abc
+
 
 class MenuElement:
     """
     A menu element, represents anything the store offers.
+    Has is_xxxx() methods for testing.
     """
+
     def __init__(self, name):
         self.name = name
 
@@ -18,19 +22,29 @@ class MenuElement:
     def is_ingredient(self):
         return self.__class__ == Ingredient
 
+    @abc.abstractmethod
+    def has_tag(self, tag):
+        return False
+
 
 class Food(MenuElement):
     """
     Food. Menu element.
     """
+
     def __init__(self, name):
         super(Food, self).__init__(name)
+
+    @abc.abstractmethod
+    def has_tag(self, tag):
+        return False
 
 
 class Pizza(Food):
     """
     A pizza. It's a Food MenuElement. Has ingredients and a size.
     """
+
     def __init__(self, name, size):
         super(Pizza, self).__init__(name)
         self.size = size
@@ -41,14 +55,22 @@ class Pizza(Food):
         Add given ingredient to the pizza.
         :param ingredient: Ingredient object to add
         :type ingredient: Ingredient
+        :return self for chaining
         """
         self.ingredients.append(ingredient)
+
+    def has_tag(self, tag):
+        for i in self.ingredients:
+            if i.has_tag(tag):
+                return True
+        return False
 
 
 class Ingredient:
     """
     An ingredient. Currently only used for pizzas, but could be used in any other type of food.
     """
+
     def __init__(self, name):
         self.name = name
         self.tags = []
@@ -57,23 +79,33 @@ class Ingredient:
         self.tags.__contains__(tag)
 
     def add_tag(self, tag):
+        """
+        Add a tag to this ingredient.
+        :param tag: A tag
+        :return: self for chaining
+        """
         self.tags.append(tag)
-        return self.tags
+        return self
 
 
 class Drink(MenuElement):
     """
     Drink. Menu element.
     """
+
     def __init__(self, name, size):
         super(Drink, self).__init__(name)
         self.size = size
+
+    def has_tag(self, tag):
+        return False
 
 
 class PricedElement:
     """
     A set of MenuElements with an associated price.
     """
+
     def __init__(self, menu_elements, price):
         """
         Constructor.
---
+++ model/Request.py
@@ -0,0 +1,60 @@
+import abc
+from abc import ABCMeta
+
+from model.Menu import MenuElement
+
+
+class Request:
+    """
+    A request emitted by a user. Supports tag constraints.
+    """
+
+    def __init__(self):
+        self.constraints = []
+
+    # TODO: Implement generic requests
+
+    def add_constraint(self, cons):
+        """
+        Add a constraint to this request.
+        :param cons: A constraint
+        :type cons: Constraint
+        :return: self for chaining
+        """
+        self.constraints.append(cons)
+        return self
+
+
+class Constraint:
+    """
+    An abstract Constraint. Implements common functionality for constraints
+    """
+    __metaclass__ = ABCMeta
+
+    @abc.abstractmethod
+    def eval(self, item):
+        """
+        Accepts or rejects given item according to the Constraint rules.
+        :param item: A menu element to evaluate
+        :type item: MenuElement
+        :return: True if accepts, False otherwise
+        """
+        return True
+
+
+class RequireTagConstraint(Constraint):
+    def __init__(self, tag):
+        super(RequireTagConstraint, self).__init__()
+        self.tag = tag
+
+    def eval(self, item):
+        return item.has_tag(item)
+
+
+class RejectTagConstraint(Constraint):
+    def __init__(self, tag):
+        super(RejectTagConstraint, self).__init__()
+        self.tag = tag
+
+    def eval(self, item):
+        return not item.has_tag(item)
